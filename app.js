75913857fc44e97487c5b170e6463c63434b740e
Nikita P <petrowitch.nickita2018@yandex.ru>
2025-02-01 02:55:19 +0300

    navbar

---
+++ app.js
@@ -0,0 +1,49 @@
+// const navToggle = document.querySelector('.nav-toggle');
+// const links = document.querySelector('.links')
+
+// navToggle.addEventListener('click', function(){
+//     // if  (links.classList.contains('chow-links')) {
+//     //     links.classList.remove('show-links')
+//     // }
+//     // else {
+//     //     links.classList.add('show-links')
+//     // }
+//     links.classList.toggle('show-links')
+// })
+
+// let x = 0;
+
+
+// const logOR = (one, two) => {
+//     if (one || two) {
+//         console.log('logOR')
+//     }
+// }
+
+// const logAnd = (one, two) => {
+//     if (one && two) {
+//         console.log('logAnd')
+//     }
+// }
+
+// logOR(x, y)
+// logAnd(x, y)
+
+// const details = {
+//     message: 'Some message'
+// }
+
+// function getMessage (count) {
+//     return this.message + count;
+// }
+
+// console.log(getMessage.apply(details, 4))
+
+// const arr = [1, 2, 3]
+// const myString = 'My super string'
+
+// arr.something = 5
+// myString.something = 5
+
+// console.log(arr.something)
+// console.log(myString.something)
